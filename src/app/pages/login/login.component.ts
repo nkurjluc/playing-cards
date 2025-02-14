aae886b1db64832c010b42f10648a27d92c3ff78
nkurjluc <nkurjluc@gmail.com>
2025-02-14 15:52:16 -0500

    Commit du 14.02.2025 après implementation du service d'authentification

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,11 @@
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { Credentials, LoginService } from '../../services/login/login.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from '../../models/user.model';
 
 
 @Component({
@@ -10,8 +14,12 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
   private formBuilder = inject(FormBuilder);
+  private loginServive = inject(LoginService);
+  private router = inject(Router);
+
+  private loginSubscription: Subscription | null = null;
   
   loginFormGroup = this.formBuilder.group({
     'username': ['',[Validators.required]],
@@ -21,7 +29,24 @@ export class LoginComponent {
   invalidCredentials=false;
 
   login(){
-    
+    this.loginSubscription = this.loginServive.login(
+      this.loginFormGroup.value as Credentials).subscribe({
+      next: (result: User | null | undefined)=> {
+        this.navigateHome();
+      },
+      error: error =>{
+        console.log(error);
+        this.invalidCredentials = true;
+      }
+    })
+  }
+
+  navigateHome(){
+    this.router.navigate(['/home']);
+  }
+
+  ngOnDestroy(): void {
+      this.loginSubscription?.unsubscribe();
   }
 
 }
